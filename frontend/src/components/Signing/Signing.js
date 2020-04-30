90d0e0b4442d45dcc84dd1fa3b0209a0932b36bb
Ben Whitmore <ben.whitmore@walmart.com>
2020-04-30 03:07:49 -0400

    adding signing component

--- frontend/src/components/Signing/Signing.js
+++ frontend/src/components/Signing/Signing.js
@@ -1,37 +1,87 @@
 import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 function Signing() {
+
+  let none = null
   const [contractlength] = React.useState([
     {label: "One Year", value: 1},
     {label: "Two Years", value: 2},
     {label: "Three Years", value: 3},
     {label: "Four Years", value: 4},
-    {label: "Five Years", value: 5},
+    {label: "Five Years", value: 5}
   ]);
-
-    const [contractrenewal] = React.useState([
+  const [contractrenewal] = React.useState([
     {label: "No", value: "no"},
     {label: "Non-Repeat", value: "non-repeat"},
     {label: "Repeat", value: "repeat"},
   ]);
+  //Make this a conditional select based on contract length?
+  const [contractteamoption] = React.useState([
+    {label: "None", value: []},
+    {label: "Year One", value: 1},
+    {label: "Year Two", value: 2},
+    {label: "Year Three", value: 3},
+    {label: "Year Four", value: 4},
+  ]);
+  //Make this a conditional select based on contract length?
+  const [contractplayeroption] = React.useState([
+    {label: "None", value: []},
+    {label: "Year One", value: 1},
+    {label: "Year Two", value: 2},
+    {label: "Year Three", value: 3},
+    {label: "Year Four", value: 4},
+  ]);
+
+  const[length, setLength] = useState([]);
+  const[renewal, setRenewal] = useState([]);
+  const[teamoption, setTeamOption] = useState([]);
+  const[playeroption, setPlayerOption] = useState([]);
+
+  let lengthdropdown = contractlength.map(contractlength => (
+    <option key={contractlength.value} value={contractlength.value}>
+    {contractlength.label}
+    </option>
+));
+  let renewaldropdown = contractrenewal.map(contractrenewal => (
+    <option key={contractrenewal.value} value={contractrenewal.value}>
+    {contractrenewal.label}
+    </option>
+));
+  let teamoptiondropdown = contractteamoption.map(contractteamoption => (
+    <option key={contractteamoption.value} value={contractteamoption.value}>
+    {contractteamoption.label}
+    </option>
+));
+  let playeroptiondropdown = contractplayeroption.map(contractplayeroption => (
+    <option key={contractplayeroption.value} value={contractplayeroption.value}>
+    {contractplayeroption.label}
+    </option>
+));
 
   return (
     <div>
-      <select>
-      {contractlength.map(contractlength => (
-        <option key={contractlength.value} value={contractlength.value}>
-          {contractlength.label}
-        </option>
-      ))}
-    </select>
 
-    <select>
-      {contractlength.map(contractlength => (
-        <option key={contractlength.value} value={contractlength.value}>
-          {contractlength.label}
-        </option>
-      ))}
-    </select>
+        <select onChange={e => setLength(e.currentTarget.value)}>
+        <option>Select Contract Length</option>
+                {lengthdropdown}
+        </select>
+
+        <select onChange={e => setRenewal(e.currentTarget.value)}>
+        <option>Select Renewal</option>
+                {renewaldropdown}
+        </select>
+
+        <select onChange={e => setTeamOption(e.currentTarget.value)}>
+        <option>Select Team Option</option>
+                {teamoptiondropdown}
+        </select>
+
+        <select onChange={e => setPlayerOption(e.currentTarget.value)}>
+        <option>Select Player Option</option>
+                {playeroptiondropdown}
+        </select>
+
       </div>
   );
 }
