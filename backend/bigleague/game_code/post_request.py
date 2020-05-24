951baaf0f89f895f2701fc315bfe9be5e6cbb07e
Ben Whitmore <ben.whitmore@walmart.com>
2020-05-24 16:44:46 -0400

    discovered wierd dataframe issue, None vs NaN when limit 1 is in sql

--- backend/bigleague/game_code/post_request.py
+++ backend/bigleague/game_code/post_request.py
@@ -5,15 +5,16 @@ import json
 
 # get database df and post request results
 conn = sqlite3.connect('/Users/buw0017/projects/TheBigLeagueGame/backend/TheBigLeagueGame.sqlite3')
-df = pd.read_sql_query("select * from players limit 20", conn)
+# dataframe shows None when limit 1 but NaN when not limit
+df = pd.read_sql_query("select * from players limit 1", conn)
 conn.close()
 
 # post request, can only do one at a time for some reason (post request doesn't accept list)
 d = df.to_dict(orient='records')
 headers = {'Content-type': 'application/json'}
 url = 'http://127.0.0.1:8000/api/players/'
-j = json.dumps(d[7])
 
+j = json.dumps(d[0])
 requests.post(url, data=j, headers=headers)
 
 # get request and change to dataframe
