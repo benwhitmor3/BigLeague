5a2bdf5597624935445a77fa33abe6025efc83f4
Ben Whitmore <ben.whitmore@walmart.com>
2020-08-28 00:32:30 -0400

    finalizing draft table

--- frontend/src/components/Draft.tsx
+++ frontend/src/components/Draft.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PlayersTable from "./Table/PlayersTable";
 import DraftTable from "./Table/DraftTable";
+import { Row, Col } from 'antd';
+import SigningFreeAgents from "./Signing/SigningFreeAgents";
 
 
 export default function Draft() {
@@ -8,9 +10,11 @@ export default function Draft() {
 
     return (
       <div>
-
-
-        <DraftTable/>
+    <Row>
+      <Col span={8}>
+                  <DraftTable/>
+      </Col>
+    </Row>
       {/*<PlayersTable/>*/}
 
       </div>
--- frontend/src/components/Table/DraftTable.tsx
+++ frontend/src/components/Table/DraftTable.tsx
@@ -4,23 +4,35 @@ import { Table, Tag, Space } from 'antd';
 
 export default function DraftTable() {
 
+  type playerConfig = {
+    name: string;
+    age: number;
+    epv: number;
+    suit: string[] | string;
+};
+
+  const draft = (player: playerConfig) => {
+    console.log(player)
+  }
 
   const columns = [
   {
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
-    render: (text: string) => <a href="/Home">{text}</a>,
+    // render: (text: string) => <a href="/Home">{text}</a>,
   },
   {
     title: 'Age',
     dataIndex: 'age',
     key: 'age',
+    sorter: (a: any, b: any) => a.age - b.age,
   },
   {
-    title: 'PV',
-    dataIndex: 'pv',
-    key: 'pv',
+    title: 'EPV',
+    dataIndex: 'epv',
+    key: 'epv',
+    sorter: (a: any, b: any) => a.epv - b.epv,
   },
   {
     title: 'Suit',
@@ -30,34 +42,60 @@ export default function DraftTable() {
       <>
         {suit.map(suit => {
           let colour;
+          let suit_icon;
           if (suit === 'diamond') {
             colour = '#40a9ff';
+            suit_icon = <span role="img" aria-label="diamond"> ♦ </span>;
           }
           else if (suit === 'spade') {
             colour = '#ffc53d';
+            suit_icon = <span role="img" aria-label="spade"> ♠ </span>;
           }
           else if (suit === 'heart') {
             colour = '#ff4d4f';
+            suit_icon = <span role="img" aria-label="heart"> ♥ </span>;
           }
           else if (suit === 'club') {
             colour = '#73d13d';
+            suit_icon = <span role="img" aria-label="club"> ♣ </span>;
           }
           return (
-            <Tag color={colour} key={suit}>
-              {suit.toUpperCase()}
+            <Tag icon={suit_icon} color={colour} key={suit}>
+             {suit.toUpperCase()}
             </Tag>
           );
         })}
       </>
     ),
+    filters: [
+      {
+        text: 'Diamond',
+        value: 'diamond',
+      },
+      {
+        text: 'Spade',
+        value: 'spade',
+      },
+      {
+        text: 'Heart',
+        value: 'heart',
+      },
+      {
+        text: 'Club',
+        value: 'club',
+      },
+    ],
+    onFilter: (value: string | number | boolean, record: any) => record.suit.indexOf(value) === 0,
   },
   {
     title: 'Action',
     key: 'action',
-    render: (text: string, record: any) => (
+    render: (text: string, record: playerConfig) => (
       <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
+        <Tag icon={<span role="img" aria-label="player"> 📝 </span>} color={'#a7a7a7'}
+             onClick={() => draft({name: record.name, age: record.age, epv: record.epv, suit: record.suit[0]})}>
+          Draft Prospect
+        </Tag>
       </Space>
     ),
   },
@@ -65,38 +103,130 @@ export default function DraftTable() {
 
 const data = [
   {
-    key: '1',
     name: 'Lebron James',
-    age: 35,
-    pv: 20,
+    age: 25,
+    epv: 25,
     suit: ['diamond'],
   },
   {
-    key: '2',
     name: 'Lebron James',
-    age: 35,
-    pv: 20,
+    age: 25,
+    epv: 17,
     suit: ['spade'],
   },
   {
-    key: '3',
     name: 'Lebron James',
-    age: 35,
-    pv: 20,
+    age: 21,
+    epv: 28,
     suit: ['heart'],
   },
   {
-    key: '4',
     name: 'Lebron James',
-    age: 35,
-    pv: 20,
+    age: 30,
+    epv: 20,
     suit: ['club'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 25,
+    suit: ['diamond'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 17,
+    suit: ['spade'],
+  },
+  {
+    name: 'Lebron James',
+    age: 21,
+    epv: 28,
+    suit: ['heart'],
+  },
+  {
+    name: 'Lebron James',
+    age: 30,
+    epv: 20,
+    suit: ['club'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 25,
+    suit: ['diamond'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 17,
+    suit: ['spade'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 25,
+    suit: ['diamond'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 17,
+    suit: ['spade'],
+  },
+  {
+    name: 'Lebron James',
+    age: 21,
+    epv: 28,
+    suit: ['heart'],
+  },
+  {
+    name: 'Lebron James',
+    age: 30,
+    epv: 20,
+    suit: ['club'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 25,
+    suit: ['diamond'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 17,
+    suit: ['spade'],
+  },
+  {
+    name: 'Lebron James',
+    age: 21,
+    epv: 28,
+    suit: ['heart'],
+  },
+  {
+    name: 'Lebron James',
+    age: 30,
+    epv: 20,
+    suit: ['club'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 25,
+    suit: ['diamond'],
+  },
+  {
+    name: 'Lebron James',
+    age: 25,
+    epv: 17,
+    suit: ['spade'],
   },
 ];
 
   return(
 
-<Table columns={columns} dataSource={data} />
+<Table columns={columns} dataSource={data} pagination={false}/>
 
   );
 }
