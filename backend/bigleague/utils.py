2f9e54cac197c39acb6d9a00d6223ca758156fbd
Ben Whitmore <bwhitmore6@bloomber.net>
2023-03-25 19:04:51 -0400

    cleanup pr

--- backend/bigleague/utils.py
+++ backend/bigleague/utils.py
@@ -2,7 +2,6 @@ from bigleague.serializers import UserSerializer
 
 
 def my_jwt_response_handler(token, user=None, request=None):
-    print("did this run??")
     return {
         'token': token,
         'user': UserSerializer(user, context={'request': request}).data
